717d2a559b8597249271775f7994e8c5496324e2
Joshua David Gardner <76405317+jgcoding2020@users.noreply.github.com>
2021-06-05 20:39:43 -0400

    ADevTG3 Homepage Template

---
+++ script.js
@@ -0,0 +1,21 @@
+$("body").css({ 
+  "background": "url(cool-background.png) no-repeat center center fixed", 
+  "-webkit-background-size": "cover",
+  "-moz-background-size": "cover",
+  "-o-background-size": "cover",
+  "background-size": "cover",
+});
+
+$("h1").css({
+    "font-size": "200%",
+    "color": "black",
+});
+
+$("header").css({
+    "font-size": "300%",
+    "color": "black",
+});
+
+$("em").css({
+    "font-size": "200%",
+});
